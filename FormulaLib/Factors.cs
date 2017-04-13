9b013a2b02c3e1620fc9f7c22e4b4a79eacdac4a
Valentin Vasilev <valentin.v1997@gmail.com>
2017-04-13 23:20:24 +0300

    Added Interest, Discount and Annuity factors methods

---
+++ FormulaLib/Factors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Math;
+
+namespace Finance
+{
+    public class Factors
+    {
+        public static double CompInterestFactor(int years, double interestRate)
+        {
+            interestRate /= interestRate <= 0.99 && interestRate <= -0.99 ? 1 : 100;
+            return Round(Pow(1 + interestRate, years), 3);
+        }
+
+        public static double DiscountFactor(int years, double interestRate) => Round(1 / CompInterestFactor(years, interestRate), 3);
+
+        public static double DiscountAnnuityFactor(int years, double interestRate)
+        {
+            interestRate /= interestRate <= 0.99 && interestRate <= -0.99 ? 1 : 100;
+
+            return Round(1 / interestRate - 1 / (interestRate * CompInterestFactor(years, interestRate)), 3);
+        }
+    }
+}
