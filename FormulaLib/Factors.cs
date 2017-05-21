fdfca12afcb674d09098e2db9f1268b00edcb6b2
Valentin Vasilev <valentin.v1997@gmail.com>
2017-05-21 23:54:17 +0300

    Added table view to the annuity factors

--- FormulaLib/Factors.cs
+++ FormulaLib/Factors.cs
@@ -6,6 +6,8 @@ namespace Finance
 {
     public class Factors
     {
+        public delegate double Method(int years, double interestRate);
+
         public static readonly string[] Attributes = { "Years", "Interest rate" }; 
 
         public static double CompInterestFactor(int years, double interestRate)
@@ -30,14 +32,29 @@ namespace Finance
             return Round(1 / interestRate - 1 / (interestRate * CompInterestFactor(years, interestRate)), 3);
         }
 
+        public static string GetTable(int years, double interestRate, Method A, Method B)               // Using delegate just to experiment
+        {
+            Table table = Table.Create(3, 3, true, false);
+            table.Modify(0, 1) = "Start Period";
+            table.Modify(0, 2) = "End Period";
+            table.Modify(1, 0) = "AF";
+            table.Modify(1, 1) = (Round(A(years, interestRate) * (1 + interestRate), 3)).ToString();
+            table.Modify(1, 2) = A(years, interestRate).ToString();
+            table.Modify(2, 0) = "DAF";
+            table.Modify(2, 1) = (Round(B(years, interestRate) * (1 + interestRate), 3)).ToString();
+            table.Modify(2, 2) = B(years, interestRate).ToString();
+
+            return table.ToString();
+        }
+
         public static string ShowFactors(int years, double interestRate)
         {
             try
             {
                 return $"Compound interest factor: {CompInterestFactor(years, interestRate)}\n" +
-                        $"Discount interest factor: {DiscountFactor(years, interestRate)}\n" +
-                        $"Annuity factor: {AnnuityFactor(years, interestRate)}\n" +
-                        $"Discount annuity factor: {DiscountAnnuityFactor(years, interestRate)}";
+                        $"Discount interest factor: {DiscountFactor(years, interestRate)}\n\n" +
+                        $"Annuity and Discount Annuity factors:\n{GetTable(years, interestRate, AnnuityFactor, DiscountAnnuityFactor)}";
+                        
             }
             catch (OverflowException)
             {
